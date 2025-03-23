d22df3b302b8d13afc02f4b2dfad5973ed3226fc
EselLuigi <luferreiro@uade.edu.ar>
2025-03-23 01:16:14 -0300

    Carga Datos1

--- Matrices.py
+++ Matrices.py
@@ -1,3 +1,35 @@
-print("SegundaPrueba")
-print("SegundaPrueba")
-print("TerceraPrueba")
+#FILAS DE LIBROS (NOMBRE / AUTOR / ID):
+#Terror:
+peli_terror1=["IT","Stephen King","001"]
+peli_terror2=["El exorcista","William Peter Blatty","002"]
+peli_terror3=["Drácula ","Bram Stoker","003"]
+peli_terror4=["El resplandor ","Stephen King","004"]
+peli_terror5=["Frankenstein","Mary Shelley","005"]
+
+#ROMANCE
+peli_romance1=["Orgullo y prejuicio","Jane Austen","101"]
+peli_romance2=["Bajo la Misma Estrella","John Green","102"]
+peli_romance3=["Cumbres borrascosas","Emily Brontë","103"]
+peli_romance4=["Posdata: Te amo","Cecelia Ahern","104"]
+peli_romance5=["Un paseo para recordar","Nicholas Sparks","105"]
+
+#ACCION 
+peli_accion1=["Los juegos del hambre","Suzanne Collins","201"]
+peli_accion2=["La isla del tesoro","Robert Louis Stevenson","202"]
+peli_accion3=["El Código Da Vinci","Dan Brown","203"]
+peli_accion4=["Jurassic Park","Michael Crichton","204"]
+peli_accion5=["Misión Imposible","Bruce Geller","205"]
+
+#POLICIAL
+peli_policial1=["Asesinato en el Orient Express","Agatha Christie","301"]
+peli_policial2=["El Sabueso de los Baskerville","Arthur Conan Doyle","302"]
+peli_policial3=["La chica del tren","Paula Hawkins","303"]
+peli_policial4=["Los hombres que no amaban a las mujeres","Stieg Larsson","304"]
+peli_policial5=["El silencio de los corderos","Thomas Harris","305"]
+
+#FANTASIA
+peli_fantasia1=["El Señor de los Anillos","J.R.R. Tolkien","401"]
+peli_fantasia2=["Harry Potter y la piedra filosofal","J.K. Rowling","402"]
+peli_fantasia3=["Crónica del asesino de reyes (El nombre del viento)","Patrick Rothfuss","403"]
+peli_fantasia4=["Juego de tronos","George R.R. Martin","404"]
+peli_fantasia5=["Alicia en el país de las maravillas","Lewis Carroll","405"]
