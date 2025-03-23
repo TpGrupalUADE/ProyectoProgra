2d1e06c1c6539f56d556403d183646bda289cd24
EselLuigi <luferreiro@uade.edu.ar>
2025-03-23 01:58:00 -0300

    CargaLibro1

--- MatrizLibro.py
+++ MatrizLibro.py
@@ -1,39 +1,38 @@
 #FILAS DE LIBROS (NOMBRE / AUTOR / ID):
-#Terror:
-libro_terror1=["IT","Stephen King","001"]
-libro_terror2=["El exorcista","William Peter Blatty","002"]
-libro_terror3=["Drácula ","Bram Stoker","003"]
-libro_terror4=["El resplandor ","Stephen King","004"]
-libro_terror5=["Frankenstein","Mary Shelley","005"]
+# Crear una matriz 2x3 con los datos
+matriz = [
+    ["IT", "Sthepen King", "001"],
+    ["El exorcista", "William Peter Blatty", "002"],
+    ["Drácula", "Bram Stoker", "003"],
+    ["El resplandor", "Stephen King", "004"],
+    ["Frankenstein", "Mary Shelley", "005"],
+    ["El silencio de los corderos", "Thomas Harris", "305"],
+    ["Orgullo y prejuicio", "Jane Austen", "101"],
+    ["Bajo la Misma Estrella", "John Green", "102"],
+    ["Cumbres borrascosas", "Emily Brontë", "103"],
+    ["Posdata: Te amo", "Cecelia Ahern", "104"],
+    ["Un paseo para recordar", "Nicholas Sparks", "105"],
+    ["Los juegos del hambre", "Suzanne Collins", "201"],
+    ["La isla del tesoro", "Robert Louis Stevenson", "202"],
+    ["El Código Da Vinci", "Dan Brown", "203"],
+    ["Jurassic Park", "Michael Crichton", "204"],
+    ["Misión Imposible", "Bruce Geller", "205"],
+    ["Asesinato en el Orient Express", "Agatha Christie", "301"],
+    ["El Sabueso de los Baskerville", "Arthur Conan Doyle", "302"],
+    ["La chica del tren", "Paula Hawkins", "303"],
+    ["Los hombres que no amaban a las mujeres", "Stieg Larsson", "304"],
+    ["El Señor de los Anillos", "J.R.R. Tolkien", "401"],
+    ["Harry Potter y la piedra filosofal", "J.K. Rowling", "402"],
+    ["Crónica del asesino de reyes (El nombre del viento)", "Patrick Rothfuss", "403"],
+    ["Juego de tronos", "George R.R. Martin", "404"],
+    ["Alicia en el país de las maravillas", "Lewis Carroll", "405"]
+]
 
-#ROMANCE
-libro_romance1=["Orgullo y prejuicio","Jane Austen","101"]
-libro_romance2=["Bajo la Misma Estrella","John Green","102"]
-libro_romance3=["Cumbres borrascosas","Emily Brontë","103"]
-libro_romance4=["Posdata: Te amo","Cecelia Ahern","104"]
-libro_romance5=["Un paseo para recordar","Nicholas Sparks","105"]
+max_len = max(len(str(num)) for fila in matriz for num in fila)
 
-#ACCION 
-libro_accion1=["Los juegos del hambre","Suzanne Collins","201"]
-libro_accion2=["La isla del tesoro","Robert Louis Stevenson","202"]
-libro_accion3=["El Código Da Vinci","Dan Brown","203"]
-libro_accion4=["Jurassic Park","Michael Crichton","204"]
-libro_accion5=["Misión Imposible","Bruce Geller","205"]
-
-#POLICIAL
-libro_policial1=["Asesinato en el Orient Express","Agatha Christie","301"]
-libro_policial2=["El Sabueso de los Baskerville","Arthur Conan Doyle","302"]
-libro_policial3=["La chica del tren","Paula Hawkins","303"]
-libro_policial4=["Los hombres que no amaban a las mujeres","Stieg Larsson","304"]
-libro_policial5=["El silencio de los corderos","Thomas Harris","305"]
-
-#FANTASIA
-libro_fantasia1=["El Señor de los Anillos","J.R.R. Tolkien","401"]
-libro_fantasia2=["Harry Potter y la piedra filosofal","J.K. Rowling","402"]
-libro_fantasia3=["Crónica del asesino de reyes (El nombre del viento)","Patrick Rothfuss","403"]
-libro_fantasia4=["Juego de tronos","George R.R. Martin","404"]
-libro_fantasia5=["Alicia en el país de las maravillas","Lewis Carroll","405"]
-
-
-#MATRIZ LIBRO
-libro=[]
+#Mostrar la matriz
+for fila in matriz:
+    for elemento in fila:
+        # Formatear cada elemento con el tamaño adecuado
+        print(f"{elemento:{max_len}}", end=" ")                     #El max_len calcula los espacios entre dato y otro para obtener la misma separación.
+    print()  # Salto de línea después de cada fila
