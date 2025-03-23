46f9668a9ac53f5936483636254339b61b88606f
EselLuigi <luferreiro@uade.edu.ar>
2025-03-23 01:29:09 -0300

    CargaDatos2

---
+++ MatrizLibro.py
@@ -0,0 +1,39 @@
+#FILAS DE LIBROS (NOMBRE / AUTOR / ID):
+#Terror:
+libro_terror1=["IT","Stephen King","001"]
+libro_terror2=["El exorcista","William Peter Blatty","002"]
+libro_terror3=["Drácula ","Bram Stoker","003"]
+libro_terror4=["El resplandor ","Stephen King","004"]
+libro_terror5=["Frankenstein","Mary Shelley","005"]
+
+#ROMANCE
+libro_romance1=["Orgullo y prejuicio","Jane Austen","101"]
+libro_romance2=["Bajo la Misma Estrella","John Green","102"]
+libro_romance3=["Cumbres borrascosas","Emily Brontë","103"]
+libro_romance4=["Posdata: Te amo","Cecelia Ahern","104"]
+libro_romance5=["Un paseo para recordar","Nicholas Sparks","105"]
+
+#ACCION 
+libro_accion1=["Los juegos del hambre","Suzanne Collins","201"]
+libro_accion2=["La isla del tesoro","Robert Louis Stevenson","202"]
+libro_accion3=["El Código Da Vinci","Dan Brown","203"]
+libro_accion4=["Jurassic Park","Michael Crichton","204"]
+libro_accion5=["Misión Imposible","Bruce Geller","205"]
+
+#POLICIAL
+libro_policial1=["Asesinato en el Orient Express","Agatha Christie","301"]
+libro_policial2=["El Sabueso de los Baskerville","Arthur Conan Doyle","302"]
+libro_policial3=["La chica del tren","Paula Hawkins","303"]
+libro_policial4=["Los hombres que no amaban a las mujeres","Stieg Larsson","304"]
+libro_policial5=["El silencio de los corderos","Thomas Harris","305"]
+
+#FANTASIA
+libro_fantasia1=["El Señor de los Anillos","J.R.R. Tolkien","401"]
+libro_fantasia2=["Harry Potter y la piedra filosofal","J.K. Rowling","402"]
+libro_fantasia3=["Crónica del asesino de reyes (El nombre del viento)","Patrick Rothfuss","403"]
+libro_fantasia4=["Juego de tronos","George R.R. Martin","404"]
+libro_fantasia5=["Alicia en el país de las maravillas","Lewis Carroll","405"]
+
+
+#MATRIZ LIBRO
+libro=[]
