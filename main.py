de8c18feaf85ce8299e890681dc2e3841f51bd06
nalcorta00 <nalcorta@uade.edu.ar>
2025-04-10 21:59:59 -0300

    1.3.4

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ matrizBibliotecaSocios = MatrizSocios.biblioteca_usuarios
 
 login.menu_login()
 
-
-print("prueba")
-opcion = int(input("Ingrese su opción: "))
-while opcion < 0 or opcion > 5:
+opcionLogin = int(input("Ingrese su opción: "))
+while opcionLogin < 0 or opcionLogin > 5:
     print("Opción inválida. Por favor, elija una opción válida.")
-    opcion = int(input("Ingrese su opción: "))
+    opcionLogin = int(input("Ingrese su opción: "))
 
-while opcion != 0:
-    if opcion == 1:
+while opcionLogin != 0:
+    if opcionLogin == 1:
         nombre_user = input("Ingresar Nombre de Usuario: ")
         
         contraseña_user = input("Ingresar Contraseña: ")
@@ -29,12 +27,12 @@ while opcion != 0:
         print("✅ Cuenta creada con éxito:")
         print(f"Usuario: {nombre_user} | Email: {nuevo_email}")
 
-    elif opcion == 2:
+    elif opcionLogin == 2:
         if login.iniciar_sesion(matrizBibliotecaSocios):
 
 #Cerrar bucle de inicio de sesion!!!
-
             login.menu()
+            opcion = int(input("Ingrese opcion: "))
             if opcion == 1:
                 print("=" * 30)
                 print("1. Buscar libro por autor")
@@ -56,11 +54,17 @@ while opcion != 0:
                     print("Regresando al menú principal...")
                 else:
                     print("Opción inválida. Por favor, elija una opción válida.")
+            elif opcion == 2:
+                print(MatrizLibro.libros)
+            else:
+                break
 
     elif opcion == 3:
         login.iniciar_sesion(MatrizAdmin)
 
         login.menuAdmin()
+
+    opcionLogin=0
 
     """elif opcion == 2:
         print("=" * 30)
@@ -77,6 +81,5 @@ while opcion != 0:
         print("Opción inválida. Por favor, elija una opción válida.")
         opcion = int(input("Ingrese su opción: "))
     opcion = int(input("\nIngrese otra opción o 0 para salir: "))"""
-    
 
     
