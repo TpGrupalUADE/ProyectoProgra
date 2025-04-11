3aa09c4840875bae7cc1091d8bf9bce238726f37
maximoparedess <maparedes@uade.edu.ar>
2025-04-10 21:05:19 -0300

    1.2 Comienzo Login Admin

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import MatrizLibro
-import MatrizUsuarios
+import MatrizSocios
 import login
+import MatrizAdmin
 
-matrizBibliotecaUsuarios = MatrizUsuarios.biblioteca_usuarios
+matrizBibliotecaSocios = MatrizSocios.biblioteca_usuarios
 
 login.menu_login()
 
-print ("Priueba")
 opcion = int(input("Ingrese su opción: "))
 while opcion < 0 or opcion > 5:
     print("Opción inválida. Por favor, elija una opción válida.")
@@ -20,42 +20,47 @@ while opcion != 0:
         while not login.validContraseña(contraseña_user):
             contraseña_user = input("Ingresar contraseña válida: ")
 
-        nuevo_id = matrizBibliotecaUsuarios[-1][1] + 1
+        nuevo_id = matrizBibliotecaSocios[-1][1] + 1
         nuevo_email = nombre_user + "@biblio.edu.ar"
 
-        matrizBibliotecaUsuarios.append([nombre_user, nuevo_id, nuevo_email, contraseña_user])
+        matrizBibliotecaSocios.append([nombre_user, nuevo_id, nuevo_email, contraseña_user])
         print("✅ Cuenta creada con éxito:")
         print(f"Usuario: {nombre_user} | Email: {nuevo_email}")
 
     elif opcion == 2:
-        login.iniciar_sesion(matrizBibliotecaUsuarios)
+        if login.iniciar_sesion(matrizBibliotecaSocios):
 
 #Cerrar bucle de inicio de sesion!!!
 
-        #nombre_user = 
-        if opcion == 1:
-            print("=" * 30)
-            print("1. Buscar libro por autor")
-            print("2. Buscar libro por nombre")
-            print("3. Buscar libro por ID")
-            print("0. Retroceder")
-            print("=" * 30)
-        sub_opcion = int(input("Ingrese su opción: "))
-        if sub_opcion == 1:
-            print("Buscando libro por autor...")
-            # Agregar la lógica para buscar por autor
-        elif sub_opcion == 2:
-            print("Buscando libro por nombre...")
-            # Agregar la lógica para buscar por nombre
-        elif sub_opcion == 3:
-            print("Buscando libro por ID...")
-            # Agregar la lógica para buscar por ID
-        elif sub_opcion == 0:
-            print("Regresando al menú principal...")
             login.menu()
-        else:
-            print("Opción inválida. Por favor, elija una opción válida.")
-    elif opcion == 2:
+            if opcion == 1:
+                print("=" * 30)
+                print("1. Buscar libro por autor")
+                print("2. Buscar libro por nombre")
+                print("3. Buscar libro por ID")
+                print("0. Retroceder")
+                print("=" * 30)
+                sub_opcion = int(input("Ingrese su opción: "))
+                if sub_opcion == 1:
+                    print("Buscando libro por autor...")
+                # Agregar la lógica para buscar por autor
+                elif sub_opcion == 2:
+                    print("Buscando libro por nombre...")
+                # Agregar la lógica para buscar por nombre
+                elif sub_opcion == 3:
+                    print("Buscando libro por ID...")
+                # Agregar la lógica para buscar por ID
+                elif sub_opcion == 0:
+                    print("Regresando al menú principal...")
+                else:
+                    print("Opción inválida. Por favor, elija una opción válida.")
+
+    elif opcion == 3:
+        login.iniciar_sesion(MatrizAdmin)
+
+        login.menuAdmin()
+
+    """elif opcion == 2:
         print("=" * 30)
         
         print("=" * 30)
@@ -69,6 +74,7 @@ while opcion != 0:
     while opcion < 0 or opcion > 5:
         print("Opción inválida. Por favor, elija una opción válida.")
         opcion = int(input("Ingrese su opción: "))
-    opcion = int(input("\nIngrese otra opción o 0 para salir: "))
+    opcion = int(input("\nIngrese otra opción o 0 para salir: "))"""
+    
 
     
